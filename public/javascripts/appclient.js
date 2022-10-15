23054a576072cd714006069efc34c6c70f03a8e4
Fatimahbintiyasin <113253050+Fatimahbintiyasin@users.noreply.github.com>
2022-10-15 02:09:23 -0400

    week5

--- public/javascripts/appclient.js
+++ public/javascripts/appclient.js
@@ -5,3 +5,18 @@
 // Date: Oct 8, 2022
 
 console.log("it goes to ther client-side.");
+
+if(getTitle == "Inventory List")
+{
+    let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+    for(button of deleteButtons)
+    {
+        button.addEventListener('click', (event)=>{
+            if(!confirm("Are you sure?")) 
+            {
+                event.preventDefault();
+            }
+        });
+    }
+}
